f57c32267227db45377a5c4fddef3024bdc1eab6
rimcx <rimcx@3d63fe75-a90b-4577-b595-b0b07fb0d485>
2013-06-22 09:41:03 +0000

    Moved error codes here from from definitions.h
    git-svn-id: svn://svn.code.sf.net/p/partmodproject/code/trunk@40 3d63fe75-a90b-4577-b595-b0b07fb0d485

--- Partmod/disk_exception.h
+++ Partmod/disk_exception.h
@@ -5,12 +5,52 @@
 #include <exception>
 #include <sstream>
 
-// For throwing and catching exeptions
+
+// Error codes
+#define ERR_OPEN_DISK            1  // Can't open disk
+#define ERR_OPEN_FILE            2  // Can't open file
+#define ERR_CREATE_FILE          3  // Can't create file
+#define ERR_READ                 4  // Can't read from disk/partition/file
+#define ERR_WRITE                5  // Can't write to disk/partiton/file
+#define ERR_INVALID_SEEK         6  // Wrong seek
+#define ERR_PART_NOT_EXIST       7  // This error is set when too big number is passed to Disk::GetPartition()
+#define ERR_EXTENDED_NOT_FOUND   8  // Extedned partition not found
+#define ERR_RESIZE_TOO_BIG       9  // Not enough of free space?
+#define ERR_BAD_RESIZE           10 //
+#define ERR_MBR_LIMIT            11 // There are 4 primary partitions
+#define ERR_DISK_FULL            12 // No free(unpartitioned) space
+#define ERR_UNKNOWN_ERROR        13 // Something's wrong
+#define ERR_PART_TOO_BIG         14 // Partition is too big
+#define ERR_PART_TOO_SMALL       15 // Partition is too small ( <1MB ? )
+#define ERR_PRIMARY_ON_EXTENDED  16 // Priamry partition on extended partition
+#define ERR_INVALID_BACKUP       17 // Invalid backup file
+#define ERR_GPT_NOT_FOUND        18 // Probably someone called Disk::ReadGPT on non-GPT disk
+#define ERR_GET_DISK_GEOMETRY    19 // Function that gets disk geometry has failed
+#define ERR_CANNOT_SPLIT         20 // Can't split partition
+#define ERR_PARTITIONS_OVERLAP   21 // Partitions overlap each other
+#define ERR_GPT_ALREADY_EXISTS   22 // GPT partiton already exists
+#define ERR_INVALID_CHECKSUM     23
+#define ERR_NOT_THAT_BACKUP      24
+#define ERR_BPS_NOT_POW_OF_TWO   25 // "Bytes per sector" value is not a power of 2
+#define ERR_OUTSIDE_DISK         26 // Partition begins or ends outside disk
+#define ERR_OUTSIDE_EXTENDED     27 // Logical volume is outside Extended partition
+#define ERR_PENDING_NOT_EXIST    28
+#define ERR_OUT_OF_BOUNDS        29
+#define ERR_GET_DISK_SIZE        30 // Could not get disk size
+#define ERR_ALREADY_OPEN         31 // Disk is already open
+#define ERR_UNKNOWN_FSID         32
+#define ERR_BKUP_BPS_MISMATCH    33
+
+
+// Error strings
+extern const char *error_table[];
+
+
 class DiskException:public std::exception
 {
 public:
 
- int error_code; // error codes are defined in definitions.h
+ int error_code;
  std::string message;
 
 
