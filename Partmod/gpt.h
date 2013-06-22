17c23e545d41e5445bda10c269064079395f1aa1
rimcx <rimcx@3d63fe75-a90b-4577-b595-b0b07fb0d485>
2013-06-22 09:48:33 +0000

    Moved class GPTHelper here
    git-svn-id: svn://svn.code.sf.net/p/partmodproject/code/trunk@44 3d63fe75-a90b-4577-b595-b0b07fb0d485

--- Partmod/gpt.h
+++ Partmod/gpt.h
@@ -9,12 +9,12 @@
 
 #pragma pack(1)
 // GPT partition table header
-typedef struct __GPT_
+struct GPT
 {
   uint8_t  signature[8];          // "EFI PART"
   uint32_t version;               // 00 00 01 00 for version 1.00
   uint32_t header_size;           // Usually 5C 00 00 00 == 92 bytes
-  uint32_t checksum;              // Checsum (CRC32) of this header; this field must be zeroed before calculation
+  uint32_t checksum;              // Checsum (CRC32) of this header; this field must be zero before calculation
   uint32_t unknown;               // Or reserved
   uint64_t current_lba;           // Location of this header
   uint64_t backup_lba;            // Location of the backup
@@ -27,10 +27,10 @@ typedef struct __GPT_
   uint32_t entries_checksum;      // CRC32 checksum of partition entries
   uint8_t  reserved[420];         // Should be zeros
 
-} GPT;
+};
 
 // GPT partition entry
-typedef struct __GPT_PARTITION_ENTRY_
+struct GPT_ENTRY
 {
   __GUID   type_guid;              // GUID of partition type
   __GUID   unique_guid;            // GUID unique for this partition
@@ -38,9 +38,8 @@ typedef struct __GPT_PARTITION_ENTRY_
   uint64_t end_lba;                // End lba of partition
   uint64_t  flags;                 // Various flags
   uint16_t name[36];               // Partition name -- 36 UTF-16LE chars
+} ;
 
-} GPT_ENTRY;
-#pragma pack(0)
 
 
 struct GPT_SPECIFIC
@@ -49,7 +48,41 @@ struct GPT_SPECIFIC
   __GUID unique_guid;
   uint16_t name[36];
   uint64_t flags;
-
 };
+
+#pragma pack()
+
+class Disk;
+class GPTHelper
+{
+public:
+  GPTHelper(Disk *disk) {this->disk=disk;}
+
+  void ReadPartitionTables(GEN_PART);
+  void WriteChanges();
+
+  bool IsValidGPT(GPT gpt);
+
+  GPT CreateGPT(GEN_PART gpt_part);
+  GPT CreateGPT(GEN_PART gpt_part,int n_gpt_entries);
+
+/* TODO:
+ * GPT_ENTRY ReadGPTEntryFromDisk(uint32_t n);
+ * GPT_ENTRY WriteGPTEntryToDisk(const GPT_ENTRY &e,uint32_t n);
+ * bool IsBackupIdentical();
+ *
+*/
+
+protected:
+  void WriteGPT(const GPT&);
+  uint32_t WritePartitionEntries(GPT gpt);
+  void WriteBackup(GPT gpt);
+
+  GPT ReadGPTFromBackup(const GEN_PART &gpt_part);
+
+  Disk *disk;
+};
+
+
 
 #endif
